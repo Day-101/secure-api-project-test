fec0b264adaf11d780ae58adefabeb94a7a93f35
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-08-31 23:44:02 +0200

    fix: db rename

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config({ path: "./config/.env" });
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.c51xt.mongodb.net/test`,
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.c51xt.mongodb.net/secure-api-project-test`,
   )
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Failed to connect to MongoDB", err));
