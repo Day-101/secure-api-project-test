d6d9dfbb379d9dc9054c8f96bba187c5749fe026
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-08-31 21:19:05 +0200

    test: MongoDB connexion

---
+++ config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "./config/.env" });
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.c51xt.mongodb.net/test`,
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Failed to connect to MongoDB", err));
