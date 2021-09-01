b84e1452b3639bbbecdd564f2083693ba7e72e48
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-09-01 13:59:24 +0200

    feat: add login and logout routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,13 @@
-const router = require('express').Router();
-const authController = require('../controllers/auth.controller');
-const userController = require('../controllers/user.controller');
+const router = require("express").Router();
+const authController = require("../controllers/auth.controller");
+const userController = require("../controllers/user.controller");
 
 // auth
 router.post("/register", authController.signUp);
+router.post("/login", authController.signIn);
+router.get("/logout", authController.logout);
 
 // user
-router.get('/', userController.getAllUsers);
+router.get("/", userController.getAllUsers);
 
 module.exports = router;
