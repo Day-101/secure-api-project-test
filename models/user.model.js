12927e224624e87d184e3cdba5dec580538cd19c
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-09-01 13:57:57 +0200

    feat: bcrypt login

--- models/user.model.js
+++ models/user.model.js
@@ -1,33 +1,46 @@
-const mongoose = require('mongoose');
-const { isEmail } = require('validator');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
-    email:{
+    email: {
       type: String,
       required: true,
       validate: [isEmail],
       lowercase: true,
-      trim: true
+      unique: true,
+      trim: true,
     },
-    password:{
+    password: {
       type: String,
       required: true,
       max: 1024,
-      minlength: 6
-    }
+      minlength: 6,
+    },
   },
   {
-    timestamps: true
+    timestamps: true,
   }
 );
 
-userSchema.pre("save", async function(next) {
+userSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt();
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
+
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error("incorrect password");
+  }
+  throw Error("incorrect email");
+};
 
 const UserModel = mongoose.model("user", userSchema);
 
