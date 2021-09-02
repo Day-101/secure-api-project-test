7f19825ee38fd4c8768447a235e9c305b71c62b5
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-09-02 14:52:30 +0200

    test-fix: CorsOption origin:true

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL,
+  origin: true,
   credentials: true,
   allowedHeaders: ["sessionId", "Content-Type"],
   exposedHeaders: ["sessionId"],
