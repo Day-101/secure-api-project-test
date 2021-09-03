b69ad1abc66e2c801f1c58fd28dd8d918b094d11
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-09-03 14:50:27 +0200

    fix: rm requireAuth

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
 const userController = require("../controllers/user.controller");
-const authMiddleware = require("../middleware/auth.middleware");
 
 // auth
 router.post("/register", authController.signUp);
@@ -9,6 +8,6 @@ router.post("/login", authController.signIn);
 router.get("/logout", authController.logout);
 
 // user
-router.get("/", authMiddleware.requireAuth, userController.getAllUsers);
+router.get("/", userController.getAllUsers);
 
 module.exports = router;
