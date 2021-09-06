56efb92f9ea58b3097ede8b4e38e989fafed011d
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-09-06 17:04:23 +0200

    reverts last commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
